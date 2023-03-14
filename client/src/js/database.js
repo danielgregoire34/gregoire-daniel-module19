264ad8437b153e1d088253ee764f044930eb7dc4
unknown <danielgregoire34@gmail.com>
2023-03-13 21:21:52 -0400

    added put and get methods for for database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+  const openjateDB = await openDB('jate',1);
+  const writejate = openjateDB.transaction('jate','readwrite');
+  const storejate = writejate.objectStore("jate");
+  const contentjate = storejate.put({id:1,value:content});
+  const resultsjate = await contentjate;
+  console.log(resultsjate);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const openjateDB = await openDB('jate',1);
+  const writejate = openjateDB.transaction('jate','readonly');
+  const storejate = writejate.objectStore("jate");
+  const contentjate = storejate.getAll();
+  const resultsjate = await contentjate;
+  console.log(resultsjate);
+};
 
 initdb();
